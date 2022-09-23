741ceba32f6dfed322e32ed962a87f299b76456d
Ian Benton <bentonian1@gmail.com>
2022-09-22 20:19:09 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import SelectInput, {
+  SelectOption,
+} from "./components/SelectInput/SelectInput";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
+
+function App() {
+  const [value1, setValue1] = useState<SelectOption | undefined>(options[0]);
+  const [value2, setValue2] = useState<SelectOption[]>([options[0]]);
+  return (
+    <div>
+      <SelectInput
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <SelectInput
+        multiple
+        options={options}
+        value={value2}
+        onChange={(o) => setValue2(o)}
+      />
+    </div>
+  );
+}
+
+export default App;
